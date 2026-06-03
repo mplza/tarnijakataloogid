09f4234d2839a2a7d5e5ceb993b0cc41667b26ce
triinbul <triinbulogina@gmail.com>
2026-06-03 21:27:56 +0300

    Eemaldasin ebavajaliku mudeli

--- dbt_project/models/marts/mart_hinnamuutus_paeviti.sql
+++
@@ -1,6 +0,0 @@
--- Marts mudel: päevased tootehinnad eurodes ja nende muutused protsentides võrreldes eelmise päeva või viimase kirjega. 
--- Kuvab ka hinnamuutuse suuna (tõus, langus, muutuseta), mis tuleb
--- int_toode_hinnamuutus mudelist (hinna_suund arvutatakse seal).
-
-SELECT *
-FROM {{ ref('int_toode_hinnamuutus') }}
